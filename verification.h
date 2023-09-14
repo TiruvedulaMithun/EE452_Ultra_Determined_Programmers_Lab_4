f50a1ff9ebc48cc2cf5ec6adbb6b02f3a29913ea
abustillos7 <55632844+abustillos7@users.noreply.github.com>
2023-09-14 14:37:12 -0700

    Use 16 bit checksum to verify correctness of packet

--- verification.h
+++ verification.h
@@ -2,44 +2,81 @@
 
 using namespace std;
 
-static int SEQ_BITS = 32;
 static int BLOCK_SIZE = 2; // measured in bytes
 
-ushort createChecksum()//char * text)
+void createChecksum(char * text,  unsigned char * checksum)
 {
-    ushort sum, carry, checksum, tmp;
+    unsigned int sum, mask, checksum_full;
     int total_bytes;
-    char * text = "hello";
     char block [BLOCK_SIZE * 8];
 
-    int num_blocks = sizeof(text) / BLOCK_SIZE;
-    if(sizeof(text) % BLOCK_SIZE != 0) {
+    int num_blocks = (int) strlen(text) / BLOCK_SIZE;
+    if(strlen(text) % BLOCK_SIZE != 0) {
+        num_blocks++;
+    }
+    // printf("NUM: %d, text: %s\n", strlen(text), text);
+
+    sum = 0;
+    mask = 0;
+    for(int i = 0; i < num_blocks; i++) {
+        for(int j = 0; j < BLOCK_SIZE; j++) {
+            if(strlen(text) > i * BLOCK_SIZE + j)
+            {
+                sum += ((text[i*BLOCK_SIZE+j] & 0x00ff) << (8*(BLOCK_SIZE-1-j)));
+                printf("%c    %x    %x\n", text[i*BLOCK_SIZE+j], (text[i*BLOCK_SIZE+j] & 0xff), sum);
+            }
+        }
+    }
+
+    for(int x = 0; x < BLOCK_SIZE; x++) {
+        mask += 0xff << (8*x);
+    }
+
+    while(sum >> (8*BLOCK_SIZE)) {
+        sum = (sum & mask) + (sum >> (8*BLOCK_SIZE));
+    }
+
+    printf("Final sum%x\n", sum);
+    checksum_full =  (~sum + 1) & mask;
+    printf("Full:%x\n", checksum_full);
+
+    for(int y = 0; y < BLOCK_SIZE ; y++) {
+        checksum[y] = 0;
+        checksum[y] = (unsigned char) (checksum_full >> (8*(BLOCK_SIZE-1-y))) & 0x00ff;
+    }
+}
+
+bool verifyChecksum(char * text)
+{
+    unsigned int mask = 0;
+    unsigned int sum = 0;
+    int num_blocks = (int) strlen(text) / BLOCK_SIZE;
+    if(strlen(text) % BLOCK_SIZE != 0) {
         num_blocks++;
     }
 
     for(int i = 0; i < num_blocks; i++) {
         for(int j = 0; j < BLOCK_SIZE; j++) {
-            if(sizeof(text) > i * BLOCK_SIZE + j)
+            if(strlen(text) > i * BLOCK_SIZE + j)
             {
-                tmp = (text[i*BLOCK_SIZE+j] << 8*j);
-                carry += (sum + tmp) << 16;
-                sum += tmp;
-                printf("%d", carry);
+                sum += ((text[i*BLOCK_SIZE+j] & 0xff) << (8*(BLOCK_SIZE-1-j)));
+                printf("%c    %d    %x\n", text[i*BLOCK_SIZE+j], 8*(BLOCK_SIZE-1-j), sum);
             }
         }
     }
-    checksum = !sum + 1;
-    printf("%d", checksum);
 
-    // return checksum;
-    return 0;
-}
+    for(int x = 0; x < BLOCK_SIZE; x++) {
+        mask += 0xff << (8*x);
+    }
 
-bool verifyChecksum(char * text) {
-    return true;
-}
+    while(sum >> (8*BLOCK_SIZE)) {
+        sum = (sum & mask) + (sum >> (8*BLOCK_SIZE));
+    }
 
-void combinePacketInfo(char * payload, int seq_num)
-{
-    
+    printf("Final sum%x", sum);
+    if ((sum & mask) == 1) {
+        return true;
+    }
+
+    return false;
 }
